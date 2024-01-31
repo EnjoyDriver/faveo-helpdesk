1a6d22aa54e5f31b1b5b421d28dc7950859781be
Mohamed Magdy <mgdym7md@yahoo.com>
2024-01-31 18:38:16 +0200

    Relation Fix

--- app/Api/v1/ApiController.php
+++ app/Api/v1/ApiController.php
@@ -859,7 +859,7 @@ class ApiController extends Controller
     public function getHelpTopic()
     {
         try {
-            $result = $this->helptopic->with('customForm.fields')->get();
+            $result = $this->helptopic->with('customForm')->get();
 
             return response()->json(compact('result'));
         } catch (\Exception $e) {
