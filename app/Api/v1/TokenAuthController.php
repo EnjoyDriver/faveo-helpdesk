15b7398b772fb7e2ef7aaeb49e43c5f6fde7fb20
Mohamed Habib <mohamedhabibwork@gmail.com>
2024-07-04 21:38:56 +0300

    fix exp

--- app/Api/v1/TokenAuthController.php
+++ app/Api/v1/TokenAuthController.php
@@ -5,11 +5,14 @@ namespace App\Api\v1;
 use App\Http\Controllers\Common\PhpMailController;
 use App\Http\Controllers\Controller;
 use App\User;
+use DB;
+use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Validator;
 
 /**
  * -----------------------------------------------------------------------------
@@ -37,38 +40,6 @@ class TokenAuthController extends Controller
     }
 
     /**
-     * Authenticating user with username and password and retuen token.
-     *
-     * @param Request $request
-     *
-     * @return type json
-     */
-    public function authenticate(Request $request)
-    {
-        $usernameinput = $request->input('username');
-        $password = $request->input('password');
-        $field = filter_var($usernameinput, FILTER_VALIDATE_EMAIL) ? 'email' : 'user_name';
-
-        //$credentials = $request->only('email', 'password');
-
-        try {
-            if (!$token = JWTAuth::attempt([$field => $usernameinput, 'password' => $password, 'active' => 1])) {
-                return response()->json(['error' => 'invalid_credentials', 'status_code' => 401]);
-            }
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'could_not_create_token', 500]);
-        } catch (\Exception $e) {
-            $error = $e->getMessage();
-
-            return response()->json(compact('error'));
-        }
-
-        $user_id = \Auth::user();
-        // if no errors are encountered we can return a JWT
-        return response()->json(compact('token', 'user_id'));
-    }
-
-    /**
      * Get the user details from token.
      *
      * @return type json
@@ -86,7 +57,7 @@ class TokenAuthController extends Controller
             return response()->json(['token_invalid', $e->getStatusCode()]);
         } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
             return response()->json(['token_absent', $e->getStatusCode()]);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             $error = $e->getMessage();
 
             return response()->json(compact('error'));
@@ -96,9 +67,43 @@ class TokenAuthController extends Controller
     }
 
     /**
+     * Authenticating user with username and password and retuen token.
+     *
+     * @param  Request  $request
+     *
+     * @return type json
+     */
+    public function authenticate(Request $request)
+    {
+        $usernameinput = $request->input('username');
+        $password = $request->input('password');
+        $field = filter_var($usernameinput, FILTER_VALIDATE_EMAIL) ? 'email' : 'user_name';
+
+        //$credentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = Auth::guard('api')->setTTL(287971200)->attempt([
+                $field => $usernameinput, 'password' => $password, 'active' => 1
+            ])) {
+                return response()->json(['error' => 'invalid_credentials', 'status_code' => 401]);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token', 500]);
+        } catch (Exception $e) {
+            $error = $e->getMessage();
+
+            return response()->json(compact('error'));
+        }
+
+        $user_id = \Auth::guard('api')->user();
+        // if no errors are encountered we can return a JWT
+        return response()->json(compact('token', 'user_id'));
+    }
+
+    /**
      * Register a user with username and password.
      *
-     * @param Request $request
+     * @param  Request  $request
      *
      * @return type json
      */
@@ -135,7 +140,7 @@ class TokenAuthController extends Controller
     public function checkUrl(Request $request)
     {
         try {
-            $v = \Validator::make($request->all(), [
+            $v = Validator::make($request->all(), [
                 'url' => 'required|url',
             ]);
             if ($v->fails()) {
@@ -156,7 +161,7 @@ class TokenAuthController extends Controller
     public function forgotPassword(Request $request)
     {
         try {
-            $v = \Validator::make($request->all(), [
+            $v = Validator::make($request->all(), [
                 'email' => 'required|email|exists:users,email',
             ]);
             if ($v->fails()) {
@@ -171,16 +176,25 @@ class TokenAuthController extends Controller
                 $user1 = $user->email;
                 //gen new code and pass
                 $code = Str::random(60);
-                $password_reset_table = \DB::table('password_resets')->where('email', '=', $user->email)->first();
+                $password_reset_table = DB::table('password_resets')->where('email', '=', $user->email)->first();
                 if (isset($password_reset_table)) {
-                    $password_reset_table = \DB::table('password_resets')->where('email', '=', $user->email)->update(['token' => $code, 'created_at' => $date]);
+                    $password_reset_table = DB::table('password_resets')->where('email', '=',
+                        $user->email)->update(['token' => $code, 'created_at' => $date]);
                     // $password_reset_table->token = $code;
                     // $password_reset_table->update(['token' => $code]);
                 } else {
-                    $create_password_reset = \DB::table('password_resets')->insert(['email' => $user->email, 'token' => $code, 'created_at' => $date]);
+                    $create_password_reset = DB::table('password_resets')->insert([
+                        'email' => $user->email, 'token' => $code, 'created_at' => $date
+                    ]);
                 }
 
-                $this->PhpMailController->sendmail($from = $this->PhpMailController->mailfrom('1', '0'), $to = ['name' => $user->user_name, 'email' => $user->email], $message = ['subject' => 'Your Password Reset Link', 'scenario' => 'reset-password'], $template_variables = ['user' => $user->user_name, 'email_address' => $user->email, 'password_reset_link' => url('password/reset/'.$code)]);
+                $this->PhpMailController->sendmail($from = $this->PhpMailController->mailfrom('1', '0'),
+                    $to = ['name' => $user->user_name, 'email' => $user->email],
+                    $message = ['subject' => 'Your Password Reset Link', 'scenario' => 'reset-password'],
+                    $template_variables = [
+                        'user' => $user->user_name, 'email_address' => $user->email,
+                        'password_reset_link' => url('password/reset/'.$code)
+                    ]);
                 $result = 'We have e-mailed your password reset link!';
 
                 return response()->json(compact('result'));
